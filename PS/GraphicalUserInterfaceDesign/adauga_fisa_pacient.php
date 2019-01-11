344f78d07987ef52f04d1706d22d7c744ea915e4
iulian77 <iulianis@yahoo.com>
2019-01-12 00:47:13 +0200

    Scripturi

--- PS/GraphicalUserInterfaceDesign/adauga_fisa_pacient.php
+++ PS/GraphicalUserInterfaceDesign/adauga_fisa_pacient.php
@@ -3,7 +3,7 @@
 $conn = new mysqli("localhost", "mihai", "parola_mihai", "proiectsincretic");
 if ($conn->connect_error) die($conn->connect_error);
 
-$query = "INSERT INTO fise_pacienti VALUES('".$_POST['prenumepacient']."', '".$_POST['zinasterepacient']."', '".$_POST['lunanasterepacient']."', '".$_POST['stradapacient']."', '".$_POST['nrstradapacient']."', '".$_POST['blocpacient']."', '".$_POST['scarapacient']."', '".$_POST['appacient']."', '".$_POST['oraspacient']."', '".$_POST['telefonpacient']."')";
+$query = "INSERT INTO fise_pacienti VALUES('".$_POST['prenumepacient']."', '".$_POST['zinasterepacient']."', '".$_POST['lunanasterepacient']."', '".$_POST['annasterepacient']."', '".$_POST['stradapacient']."', '".$_POST['nrstradapacient']."', '".$_POST['blocpacient']."', '".$_POST['scarapacient']."', '".$_POST['appacient']."', '".$_POST['oraspacient']."', '".$_POST['telefonpacient']."', '".'CURDATE()'."')";
 $result = $conn->query($query);
 if($result){
 		echo 1;
