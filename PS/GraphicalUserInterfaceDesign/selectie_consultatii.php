4cdf2249de94daa9168de2ac0ef009a8bda18ee8
iulian77 <iulianis@yahoo.com>
2019-01-13 14:08:55 +0200

    Incercare

---
+++ PS/GraphicalUserInterfaceDesign/selectie_consultatii.php
@@ -0,0 +1,19 @@
+<?php
+//$conn = new mysqli($_POST["host"], $_POST["user"], $_POST["pass"], $_POST["dbname"]);
+$conn = new mysqli("localhost", "mihai", "parola_mihai", "proiectsincretic");
+if ($conn->connect_error) die($conn->connect_error);
+
+$query = "SELECT * FROM foi_consultatie WHERE numar_fisa = ".$_POST['pacientincasat'];
+$result = $conn->query($query);
+if($result){
+	$interventii_existente = array();
+	for($i=0; $i<$result->num_rows;$i++){
+		$result->data_seek($i);
+		$interventie = $result->fetch_array(MYSQLI_ASSOC);
+		array_push($interventii_existente, $interventie);
+	}
+}
+echo json_encode($interventii_existente);
+
+$conn->close();
+?>
